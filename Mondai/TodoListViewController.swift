80952373ae419f766a2e0e515dc376f96bbce62b
Christian Stevanus <c.stevanus@bridgetech.co.id>
2019-01-24 16:30:12 +0700

    to do list complete with no data persistance

---
+++ Mondai/TodoListViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Mondai
+//
+//  Created by Christian Stevanus on 24/01/19.
+//  Copyright © 2019 Christian Stevanus. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController
+{
+    
+    let itemArray = ["find Mike", "buy Eggsy", "beat Black giant" ]
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    //MARK - Tableview Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    //MARK - Tableview Delegate Method
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        //UI animation
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+
+}
+
